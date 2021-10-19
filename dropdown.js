c62931124148e49762ba77cfdd9056992878b3d8
Merlinkoma <ameye.melissa@gmail.com>
2021-10-19 12:14:46 +0200

    initial commit

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+document.getElementById("nav-toggle").addEventListener("click", openMenu);
+
+function openMenu(){
+    document.getElementById("dropdown-menu").classList.toggle("active");
+}
+
+// tuto github/tsampaio
