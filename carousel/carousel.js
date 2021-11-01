2c7567e760911779a66dbfe785b6f4f77afae822
Mélissa Ameye <ameye.melissa@gmail.com>
2021-11-01 10:56:12 +0100

    add carousel functions and remove .css from git

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,9 +1,44 @@
-function carousel(){
-    let slideleft = document.getElementById('slideleft');
-    for (let i = 0; i < 10; i++) {
-        slideleft.addEventListener('mouseover', function (){
-            document.getElementById('registrar-carousel').style.transform = `translateX(-${i}vw)`;
-        })
-    }  
+let container = document.getElementById('container2');
+let slideleft = document.getElementById('slideleft');
+let slideright = document.getElementById('slideright');
+slideleft.addEventListener("click", moveLeft);
+slideright.addEventListener("click", moveRight);
+
+function moveLeft(){
+    console.log("coucou gauche");
+    container.style.transform = `translateX(-50vw)`;
 }
-carousel();
+
+function moveRight(){
+    console.log("coucou droite");
+    container.style.transform = `translateX(50vw)`;
+}
+
+// for (let i = 0; i < 10; i++) {
+//     slideleft.addEventListener('mouseover', function () {
+//         document.getElementById('registrar-carousel').style.transform = `translateX(-${i}vw)`;
+//     })
+// }
+
+
+let slideIndex = 0;
+
+const showSlides = () => {
+    const slides = document.getElementsByClassName("images-div1");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000);
+};
+
+showSlides();
+
